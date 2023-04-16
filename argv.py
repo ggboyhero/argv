601c5939af296633b1706030f66e47ddd5f17d87
ggboyhero <130449163+ggboyhero@users.noreply.github.com>
2023-04-16 00:08:00 -0800

    Update argv.py

--- argv.py
+++ argv.py
@@ -14,6 +14,21 @@ def myfunc(argv):
     except:
         print(arg_help)
         sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(arg_help)  # print the help message
+            sys.exit(2)
+        elif opt in ("-i", "--input"):
+            arg_input = arg
+        elif opt in ("-u", "--user"):
+            arg_user = arg
+        elif opt in ("-o", "--output"):
+            arg_output = arg
+
+    print('input:', arg_input)
+    print('user:', arg_user)
+    print('output:', arg_output)
 
 
 if __name__ == "__main__":
